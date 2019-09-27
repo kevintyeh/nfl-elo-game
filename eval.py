c7bbf5d20a645afdcb71c51f4def22990d573614
Kevin Yeh <kevintyeh@gmail.com>
2019-09-27 16:46:45 -0400

    added general algorithm design for comparing elo ratings

--- eval.py
+++ eval.py
@@ -9,3 +9,20 @@ Forecast.forecast(games)
 
 # Evaluate our forecasts against Elo
 Util.evaluate_forecasts(games)
+
+# TODO:
+"""
+Create code to compare logic with ELO points and Yahoo spread
+
+General Algorithm:
+
+Does ELO Favorite == Yahoo Favorite:
+    If True:
+        Is ELO > Yahoo:
+            True: ELO more bullish on favorite
+                Then: Choose underdog
+            False: ELO less bullish/confident then Yahoo
+                Then: Choose favorite
+    Else:
+        Choose Favorite (ELO thinks underdog is going to win)
+"""
